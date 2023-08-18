a87e18ab904e7679050a9ef5f70085946e4fdb67
alcidi <139457132+JeasonD6V@users.noreply.github.com>
2023-08-18 21:17:25 +0300

    Add files via upload

---
+++ Multiplication Table.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int x, y;
+
+    for (x = 1; x <= 9; x += 3) {
+        for (y = 1; y <=9 ; y++) {
+            printf("%2d x %2d = %3d   ", x, y, x * y);
+            printf("%2d x %2d = %3d   ", x+1, y, (x+1) * y);
+            printf("%2d x %2d = %3d   \n", x+2, y, (x+2) * y);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
