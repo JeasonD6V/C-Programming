d9daecbf7f10abcbdefe35b13c380aacf092bd2c
alcidi <139457132+JeasonD6V@users.noreply.github.com>
2023-08-18 08:49:14 +0300

    Add files via upload

---
+++ PassFailCheck.c
@@ -0,0 +1,23 @@
+
+	#include <stdio.h>
+
+int main() {
+	
+int score;
+
+    printf("Enter your course score\n-->  ");
+    scanf("%d", &score);
+
+    if (score < 50) {
+        printf("Unfortunately, you have failed.\n");
+        
+    } else if (score >= 50) {
+    	
+        printf("Congratulations, you have passed.\n");
+    }
+
+    return 0;
+    
+}
+
+
